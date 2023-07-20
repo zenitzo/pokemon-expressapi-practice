810e023f04edf8d974bfdf79215ebedf19b2a4cf
zenitzo <christiang.dev@gmail.com>
2023-07-20 19:13:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.get("/api/:name", (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`Good job port ${PORT} is running smoothly!`);
 });
