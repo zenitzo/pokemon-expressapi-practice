8b06307eb9b80846b3c5e1b867b37eaf7f7414c5
zenitzo <christiang.dev@gmail.com>
2023-07-20 18:34:11 -0400

    'working pokemon api"
    node express practice

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+const pokemon = {
+  lugia: {
+    type: "Psychic/flying",
+  },
+  gengar: {
+    type: "Ghost/poison",
+  },
+  charizard: {
+    type: "Fire/flying",
+  },
+  swampert: {
+    type: "Water/ground",
+  },
+  unknown: {
+    type: "unknown",
+  },
+};
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/:name", (req, res) => {
+  const pokeName = req.params.name.toLowerCase();
+  if (pokemon[pokeName]) {
+    res.json(pokemon[pokeName]);
+  } else {
+    res.json(pokemon["unknown"]);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Good job port ${PORT} is running smoothly!`);
+});
